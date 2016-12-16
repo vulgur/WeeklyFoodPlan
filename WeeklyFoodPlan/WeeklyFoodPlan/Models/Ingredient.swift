97d079889f01c4b5c3eff6828662c7361be258ce
Wang Shudao <shudao.wang@me.com>
2016-12-17 01:13:03 +0800

    Add Ingredient struct

---
+++ WeeklyFoodPlan/WeeklyFoodPlan/Models/Ingredient.swift
@@ -0,0 +1,15 @@
+//
+//  Ingredient.swift
+//  WeeklyFoodPlan
+//
+//  Created by vulgur on 2016/12/17.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import UIKit
+
+struct Ingredient {
+    let name: String
+    var nutritions: [String]
+    var icon: UIImage?
+}
