22f8cc43d1af06b733df9f9e96b8dd306926ee63
Wang Shudao <shudao.wang@me.com>
2016-12-22 23:27:07 +0800

    Add RecipeSet struct

---
+++ WeeklyFoodPlan/WeeklyFoodPlan/Models/RecipeSet.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeSet.swift
+//  WeeklyFoodPlan
+//
+//  Created by vulgur on 2016/12/22.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import Foundation
+
+struct RecipeSet {
+    let name: String
+    let recipes: [Recipe]
+    let tags: [String]
+    
+}
