9d94cffd805defff3eef645c7a8c3dabef3c4929
Wang Shudao <shudao.wang@me.com>
2017-01-02 00:31:59 +0800

    Add mapping to Recipe

--- WeeklyFoodPlan/WeeklyFoodPlan/Models/Recipe.swift
+++ WeeklyFoodPlan/WeeklyFoodPlan/Models/Recipe.swift
@@ -7,21 +7,50 @@
 //
 
 import UIKit
+import ObjectMapper
 
-struct Recipe {
+struct Recipe: Mappable {
     
-    enum RecipeType {
-        case HomeCooking
-        case EatingOut
-        case TakeOut
+    enum RecipeType: String {
+        case HomeCooking = "HomeCooking"
+        case EatingOut = "EatingOut"
+        case TakeOut = "TakeOut"
+        case Other = "Other"
     }
     
-    var name: String
-    var tags: [String]
-    var type: RecipeType
-    var ingredients: [Ingredient]
-    var photo: UIImage
-    var cookCount: Int
-    var isFavored: Bool
-    var steps: [RecipeStep]
+    let typeTransform = TransformOf<RecipeType, String>(fromJSON: { (type: String?) -> Recipe.RecipeType? in
+        if let type = type {
+            switch type {
+                case "HomeCooking": return RecipeType.HomeCooking
+                case "EatingOut": return RecipeType.EatingOut
+                case "TakeOut": return RecipeType.TakeOut
+                default: return RecipeType.Other
+            }
+        } else {
+            return RecipeType.Other
+        }
+    }) { (recipeType: Recipe.RecipeType?) -> String? in
+        return recipeType?.rawValue
+    }
+    
+    var name: String?
+    var tags: [String]?
+    var type: RecipeType?
+    var ingredients: [Ingredient]?
+    var photo: UIImage?
+    var cookCount: Int?
+    var isFavored: Bool?
+    var steps: [RecipeStep]?
+    
+    init?(map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        name <- map["name"]
+        tags <- map["tags"]
+        type <- (map["type"], typeTransform)
+        ingredients <- map["ingredients"]
+        // TODO: add properties to map
+    }
 }
