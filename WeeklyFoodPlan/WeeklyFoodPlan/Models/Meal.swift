bb9746015e82457bab1343df9ee96f1bf8ac82eb
Wang Shudao <shudao.wang@me.com>
2017-03-03 01:17:18 +0800

    Add Meal and PlanMealCell and PlanFoodCell

---
+++ WeeklyFoodPlan/WeeklyFoodPlan/Models/Meal.swift
@@ -0,0 +1,27 @@
+//
+//  Meal.swift
+//  WeeklyFoodPlan
+//
+//  Created by vulgur on 2017/3/3.
+//  Copyright © 2017年 MAD. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Meal: Object {
+    dynamic var id = UUID().uuidString
+    dynamic var name: String = ""
+    var foods = List<Food>()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+class DoneFoodInMeal: Object {
+    dynamic var id = UUID().uuidString
+    dynamic var meal = Meal()
+    dynamic var food = Food()
+    dynamic var isDone = false
+}
---
+++ WeeklyFoodPlan/WeeklyFoodPlan/Views/PlanList/PlanFoodCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlanFoodCell.swift
+//  WeeklyFoodPlan
+//
+//  Created by vulgur on 2017/3/3.
+//  Copyright © 2017年 MAD. All rights reserved.
+//
+
+import UIKit
+
+class PlanFoodCell: UICollectionViewCell {
+
+    @IBOutlet var foodNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
